9c0336e9f5ee4ad19392bc3de63673c2e1ff4fa0
Garrett Martin <me@garrettqmartin.com>
2017-02-27 10:10:47 -0600

    Add functions to make turbolinks visits.

--- turbolinks-ios-spike/AppDelegate.swift
+++ turbolinks-ios-spike/AppDelegate.swift
@@ -7,17 +7,31 @@
 //
 
 import UIKit
+import Turbolinks
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var navigationController = UINavigationController()
+    var session = Session()
 
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         window?.rootViewController = navigationController
+        startApplication()
         return true
+    }
+    
+    func startApplication() {
+        session.delegate = self
+        visit(NSURL(string: "http://localhost:3000")!)
+    }
+    
+    func visit(URL: NSURL) {
+        let visitableViewController = VisitableViewController(URL: URL)
+        navigationController.pushViewController(visitableViewController, animated: true)
+        session.visit(visitableViewController)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -45,3 +59,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate: SessionDelegate {
+    func session(session: Session, didProposeVisitToURL URL: NSURL, withAction action: Action) {
+        visit(URL)
+    }
+    
+    func session(session: Session, didFailRequestForVisitable visitable: Visitable, withError error: NSError) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        navigationController.presentViewController(alert, animated: true, completion: nil)
+    }
+}
